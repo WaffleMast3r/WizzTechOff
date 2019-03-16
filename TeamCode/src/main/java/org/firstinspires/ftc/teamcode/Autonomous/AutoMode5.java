a026ff8503c42c1fbc0ef737ca98d478161eedb1
DeadShadow80 <leonnis12@yahoo.com>
2019-03-16 16:31:22 +0200

    Starting point for Final code
    created 2 new classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoMode5.java
@@ -0,0 +1,144 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.EasyRobot;
+
+@Autonomous(name = "Regional - Timisoara(up)", group = "Official")
+public class AutoMode5 extends EasyRobot {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initRobot(this);
+
+        getCollectorRotateServo().setPosition(0);
+
+        initVuforia();
+        initTfod(2);
+        initGyro(BNO055IMU.AngleUnit.DEGREES);
+
+        lock();
+
+        waitForStart();
+
+        getCollectorRotateServo().setPosition(1);
+
+        touchDown();
+
+        getCollectorRotateServo().setPosition(0.5);
+        driveForward(15, 1);
+
+        runObjectDetection(2, new ObjectDetected() {
+            @Override
+            public void pickup() {
+                drive(getHandMotor(), -20, 0.3, true);
+
+                waitForMotors(getHandMotor().getName());
+                getCollectorRotateServo().setPosition(1);
+                getCollectorServo().setPower(1);
+                drive(getHandMotor(), -20, 0.05, true);
+                waitForMotors(getHandMotor().getName());
+                getCollectorServo().setPower(0);
+                getCollectorRotateServo().setPosition(0.5);
+            }
+
+            @Override
+            public void onLeft() {
+                turnTo(0.3, 0);
+            }
+
+            @Override
+            public void onCenter() {
+                getCollectorRotateServo().setPosition(0.5);
+                sleep(500);
+                driveForward(50, 1);
+                getCollectorRotateServo().setPosition(1);
+                getCollectorServo().setPower(1);
+                driveForward(30, 0.3);
+                waitForMotors("m1", "m2", "m3", "m4");
+                getCollectorRotateServo().setPosition(0.5);
+                getCollectorServo().setPower(0);
+                driveForward(-30, 1);
+            }
+
+            @Override
+            public void onRight() {
+                turnTo(0.3, 0);
+            }
+
+
+            @Override
+            public void loadCargo() {
+                drive(getHandMotor(), 35, 0.3, true);
+                waitForMotors(getHandMotor().getName());
+                getCollectorRotateServo().setPosition(0);
+                sleep(1500);
+                getCollectorRotateServo().setPosition(0.5);
+
+                driveForward(-13, 1);
+                waitForMotors("m1", "m2", "m3", "m4");
+                driveTicks(getExtendLift(), 1240, 0.5, true, true);
+                waitForMotors(getExtendLift().getName());
+                getExtendLift().setBrake(true);
+
+                getLiftServo1().setPosition(0);
+                getLiftServo2().setPosition(1);
+
+                getExtendLift().setBrake(false);
+
+
+                getLiftServo1().setPosition(0.5);
+                getLiftServo2().setPosition(0.5);
+            }
+        });
+
+//        driveForward(50, 0.8);
+//        drive(getHandMotor(), -40, 0.5, true);
+//        waitForMotors(getHandMotor().getName());
+//        getCollectorRotateServo().setPosition(0.5);
+
+
+        sleep(5000);
+        disable();
+    }
+
+    private void lock() {
+        getExtendLift2().setBrake(true);
+        getExtendLift().setBrake(true);
+        getCollectorRotateServo().setPosition(0);
+    }
+
+    private void touchDown() {
+        getExtendLift2().setBrake(false);
+        getExtendLift().setBrake(false);
+
+        driveTicks(getExtendLift(), 1240, 0.1, true, false);
+        waitForMotors(getExtendLift().getName());
+
+        driveWith(0.3, 15, 15, -15, -15);
+        driveForward(10, 0.3);
+        driveWith(0.3, -15, -15, 15, 15);
+        driveForward(-10, 0.3);
+
+        driveTicks(getExtendLift2(), 0, 0.1, true, false);
+        waitForMotors(getExtendLift2().getName());
+    }
+
+    public void driveForward(int distance, double speed) {
+        drive(getLeftMotorUp(), -distance, speed, false);
+        drive(getRightMotorUp(), distance, speed, false);
+        drive(getLeftMotorDown(), -distance, speed, false);
+        drive(getRightMotorDown(), distance, speed, false);
+        update("m1", "m2", "m3", "m4");
+    }
+
+
+    public void driveWith(double speed, int distanceLeftUp, int distanceRightUp, int distanceLeftDown, int distanceRightDown) {
+        drive(getLeftMotorUp(), distanceLeftUp, speed, false);
+        drive(getRightMotorUp(), distanceRightUp, speed, false);
+        drive(getLeftMotorDown(), distanceLeftDown, speed, false);
+        drive(getRightMotorDown(), distanceRightDown, speed, false);
+        update("m1", "m2", "m3", "m4");
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpMode4.java
@@ -0,0 +1,119 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.EasyRobot;
+import org.firstinspires.ftc.teamcode.MotorEncoderController;
+
+
+@TeleOp(name = "TeleOp - Bucuresti", group = "Official")
+public class TeleOpMode4 extends EasyRobot {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initRobot(this);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            if (gamepad1.x) {
+                getLeftMotorUp().getMotor().setPower(-0.1);
+                getRightMotorUp().getMotor().setPower(0.1);
+                getLeftMotorDown().getMotor().setPower(0.1);
+                getRightMotorDown().getMotor().setPower(-0.1);
+            } else {
+                getLeftMotorUp().getMotor().setPower(Range.clip(-gamepad1.left_stick_x + gamepad1.left_stick_y - gamepad1.right_stick_x, -1, 1));
+                getRightMotorUp().getMotor().setPower(Range.clip(-gamepad1.left_stick_x - gamepad1.left_stick_y - gamepad1.right_stick_x, -1, 1));
+                getLeftMotorDown().getMotor().setPower(Range.clip(gamepad1.left_stick_x + gamepad1.left_stick_y - gamepad1.right_stick_x, -1, 1));
+                getRightMotorDown().getMotor().setPower(Range.clip(gamepad1.left_stick_x - gamepad1.left_stick_y - gamepad1.right_stick_x, -1, 1));
+            }
+
+            if (gamepad1.right_trigger > 0) {
+                if (getHandMotor().getMotor().getCurrentPosition() >= -2300) {
+                    getHandMotor().getMotor().setPower(-gamepad1.right_trigger);
+                } else {
+                    getHandMotor().getMotor().setPower(0);
+                }
+            } else if (gamepad1.left_trigger > 0) {
+                if (getHandMotor().getMotor().getCurrentPosition() <= 0) {
+                    getHandMotor().getMotor().setPower(gamepad1.left_trigger);
+                } else {
+                    getHandMotor().getMotor().setPower(0);
+                }
+            } else {
+                getHandMotor().getMotor().setPower(0);
+            }
+
+            if (gamepad1.left_bumper && gamepad1.right_bumper && gamepad1.a) {
+
+                driveTicks(getExtendLift(), 0, 1, true, false);
+                driveTicks(getExtendLift2(), 500, 0.3, true, false);
+
+                while (areBusyMotors(getExtendLift().getName(), getExtendLift2().getName())) {
+                    getLeftMotorUp().getMotor().setPower(Range.clip(-gamepad1.left_stick_x + gamepad1.left_stick_y - gamepad1.right_stick_x, -1, 1));
+                    getRightMotorUp().getMotor().setPower(Range.clip(-gamepad1.left_stick_x - gamepad1.left_stick_y - gamepad1.right_stick_x, -1, 1));
+                    getLeftMotorDown().getMotor().setPower(Range.clip(gamepad1.left_stick_x + gamepad1.left_stick_y - gamepad1.right_stick_x, -1, 1));
+                    getRightMotorDown().getMotor().setPower(Range.clip(gamepad1.left_stick_x - gamepad1.left_stick_y - gamepad1.right_stick_x, -1, 1));
+                }
+
+//                waitForMotors(getExtendLift().getName(), getExtendLift2().getName());
+
+                getExtendLift().setBrake(true);
+                getExtendLift2().setBrake(true);
+                while (opModeIsActive()) {
+                }
+
+                getExtendLift2().setBrake(false);
+                getExtendLift().setBrake(false);
+
+                driveTicks(getExtendLift(), 1240, 0.1, true, false);
+                waitForMotors(getExtendLift().getName());
+
+                disable();
+                MotorEncoderController.disable();
+            } else if (gamepad1.left_bumper) {
+                driveTicks(getExtendLift(), 1240, 1, true, false);
+                getExtendLift().getMotor().setPower(0);
+            } else if (gamepad1.right_bumper) {
+                driveTicks(getExtendLift2(), 0, 1, true, false);
+                getExtendLift2().getMotor().setPower(0);
+            }
+
+
+            if (gamepad2.y) {
+                if (gamepad2.left_bumper && gamepad2.right_bumper) {
+                    getLiftServo1().setPosition(0);
+                    getLiftServo2().setPosition(1);
+                } else if (gamepad2.right_bumper) {
+                    getLiftServo1().setPosition(0);
+                } else if (gamepad2.left_bumper) {
+                    getLiftServo2().setPosition(1);
+                } else {
+                    getLiftServo1().setPosition(1);
+                    getLiftServo2().setPosition(0);
+                }
+            }
+
+
+            if (gamepad2.left_bumper) {
+                getCollectorServo().setPower(1);
+            } else if (gamepad2.right_bumper) {
+                getCollectorServo().setPower(-1);
+            } else {
+                getCollectorServo().setPower(0);
+            }
+
+            if (gamepad2.dpad_left) {
+                getCollectorRotateServo().setPosition(0);
+            } else if (gamepad2.dpad_up) {
+                getCollectorRotateServo().setPosition(0.5);
+            } else if (gamepad2.dpad_right) {
+                getCollectorRotateServo().setPosition(1);
+            }
+
+        }
+    }
+}
